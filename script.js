76177fd72b32839709729a7ecc60131a758207dd
saurabhrajak9930951281-wq <saurabhrajak9930951281@gmail.com>
2026-03-30 22:12:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function showMessage() {
-    alert("Thanks for visiting my portfolio!");
-}
 
-function send() {
-    let name = document.getElementById("name").value;
-
-    if(name === "") {
-        alert("Please enter your name");
-    } else {
-        alert("Hello " + name + ", message sent!");
-    }
-}
